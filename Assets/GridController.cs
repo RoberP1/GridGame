195ac2add543b53a7bd81fcb0c6ee3c01e1d6c5c
RoberP1 <roberpapeschi@gmail.com>
2022-06-28 14:49:08 -0300

    Update GridController.cs

--- Assets/GridController.cs
+++ Assets/GridController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GridController : MonoBehaviour
 {
-    public Grid<GridObject> grid;
+    public Grid<int> grid;
     public int x, y;
     public float cellsize;
     public Vector3 origin;
@@ -12,14 +12,14 @@ public class GridController : MonoBehaviour
     public GameObject tilePrefab;
     void Awake()
     {
-        grid = new Grid<GridObject>(x, y, cellsize, origin);
-        grid.InitializeGrid(new GridObject(0,sprites[0]),new GridObject(2, sprites[2]));
+        grid = new Grid<int>(x, y, cellsize, origin);
+        grid.InitializeGrid(0,2);
         InitializeGrid();
         
     }
     private void OnEnable()
     {
-        GridMovement.OnStart += (pos, id) => grid.SetValue(pos, new GridObject(id, sprites[id]));
+        GridMovement.OnStart += (pos, id) => grid.SetValue(pos,id);
         GridMovement.OnMove += checkDirection;
     }
     private void OnDisable()
@@ -34,20 +34,20 @@ public class GridController : MonoBehaviour
     bool checkDirection(Vector3 WorldPos, Vector2 Direction, int CanMoveId, int id)
     {
         bool canMove = false;
-        GridObject neighbourId = grid.GetValue(WorldPos + (Vector3)Direction);
-        if (neighbourId.id == 0)
+        int neighbourId = grid.GetValue(WorldPos + (Vector3)Direction);
+        if (neighbourId == 0)
         {
             canMove = true;
         }
-        else if (CanMoveId != 0 && neighbourId.id == CanMoveId)
+        else if (CanMoveId != 0 && neighbourId == CanMoveId)
         {
             //empujar caja
         }
         
         if (canMove)
         {
-            grid.SetValue(WorldPos, new GridObject(0, sprites[0]));
-            grid.SetValue(WorldPos + (Vector3)Direction, new GridObject(id, sprites[id]));
+            grid.SetValue(WorldPos, 0);
+            grid.SetValue(WorldPos + (Vector3)Direction, id);
         }
         //Debug.Log("neighbourId = " + neighbourId.id);
         Debug.Log("canMove = " + canMove);
@@ -60,7 +60,7 @@ public class GridController : MonoBehaviour
             for (int y = 0; y < this.y; y++)
             {
                 GameObject tile =Instantiate(tilePrefab, grid.GetWorldPos(x, y) + (Vector3)Vector2.one * cellsize/2, Quaternion.identity);
-                tile.GetComponent<SpriteRenderer>().sprite = grid.GetValue(x, y).sprite;
+                tile.GetComponent<SpriteRenderer>().sprite = sprites[grid.GetValue(x, y)];
             }
         }
     }
